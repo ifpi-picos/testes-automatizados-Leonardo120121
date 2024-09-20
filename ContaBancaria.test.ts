7934410d66f2115320f8c778e9a6b86e17e7237a
Leonardo <leonardovitorio007@gmail.com>
2024-09-20 14:33:56 -0300

    testes automatizados

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,52 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
+describe("Teste de classe ContaBancaria", () => {
+    let conta: ContaBancaria
+    let contaDois: ContaBancaria
+    beforeEach(() => {
+        conta = new ContaBancaria()
+        contaDois = new ContaBancaria
+    })
 
+    test("Método Depositar", () => {
+
+        expect(conta.depositar(50)).toBe(50)
+    })
+
+    test("Método Depositar - deposito inválido", () => {
+        expect(() => conta.depositar(-10)).toThrow("Valor inválido")
+    })
+
+    test("Método Sacar", () => {
+        conta.depositar(50)
+        expect(conta.sacar(10)).toBe(40)
+    })
+
+    test("Método Sacar - saque inválido", () => {
+        conta.depositar(50)
+        expect(() => conta.sacar(60)).toThrow("Valor inválido")
+    })
+
+    test("Método tranferir", () => {
+        conta.depositar(500)
+        expect(conta.transferir(200, contaDois)).toBe(300)
+    })
+
+    test("Método tranferir - Valor Inválido", () => {
+        expect(() => conta.transferir(200, contaDois))
+            .toThrow("valor inválido ou saldo insuficiente")
+    })
+
+    test("Método Saldo", () => {
+        conta.depositar(10)
+        expect(conta.consultarSaldo()).toBe(10)
+    })
+
+    test("Método exibir extrato", () => {
+        conta.depositar(110)
+        conta.sacar(53)
+        expect(conta.exibirExtrato()).toBe("1. Depósito de R$ 110.00\n2. Depósito de R$ 110.00 - 20/9/2024\n3. Saque de R$ 53.00\n4. Saque de R$ 53.00 - 20/9/2024")
+        })
+    
+})
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,85 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private numeroConta: number
+    private agencia: number
+    private saldo: number
+    private extrato: string[]
+
+
+    public constructor() {
+        this.numeroConta = Math.floor(Math.random()
+        * 900000000) + 100000000
+        this.numeroConta = 0
+        this.agencia = 0
+        this.saldo = 0
+        this.extrato = []
+    }
+
+
+    public depositar(valor: number) {
+        if (valor > 0) {
+            this.saldo += valor
+            this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`)
+            let descricao =`Depósito de R$ ${valor.toFixed(2)}`
+            this.registrarOperacao(descricao)
+            return this.saldo
+        } else {
+            throw new Error("Valor inválido")
+        }
+
+    }
+
+    public sacar(valor: number) {
+        if (valor > 0 && valor <= this.saldo) {
+            this.saldo -= valor
+            this.extrato.push(`Saque de R$ ${valor.toFixed(2)}`)
+            let descricao =`Saque de R$ ${valor.toFixed(2)}`
+            this.registrarOperacao(descricao)
+            return this.saldo
+        } else {
+            throw new Error("Valor inválido")
+        }
+
+    }
+
+
+
+    private receberTransferencia(valor: number, conta: ContaBancaria) {
+        conta.saldo += valor
+        conta.extrato.push(`Transferência de ${valor} recebida`)
+        let descricao=`Transferência de ${valor} recebida`
+        conta.registrarOperacao(descricao)
+    }
+
+    public transferir(valor: number, conta: ContaBancaria) {
+        if (valor > 0 && valor <= this.saldo) {
+            conta.receberTransferencia(valor, conta)
+            this.saldo -= valor
+            this.extrato.push(`Transferêcia de r$ ${valor} realizada.`)
+            let descricao = `Transferêcia de r$ ${valor} realizada.`
+            this.registrarOperacao(descricao)
+            return this.saldo
+        } else {
+            throw new Error("valor inválido ou saldo insuficiente")
+        }
+    }
+
+    public consultarSaldo(){
+        return this.saldo
+    }
+
+    public exibirExtrato(){
+       let extratoString = " "
+
+       this.extrato.forEach((operacao, index) => {
+            extratoString += `${index + 1}. ${operacao}\n`
+       })
+       return extratoString.trim()
+    }
+    private registrarOperacao(descricao: string){
+        let data: Date | string = new Date()
+    data = `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`
+        descricao = `${descricao} - ${data}`
+        this.extrato.push(descricao)
+      
+    }
+}
